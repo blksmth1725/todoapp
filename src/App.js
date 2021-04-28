94cef3b67cf36f3243f4a83196c2c843d27bb410
Christian Sheen <csheen1725@gmail.com>
2021-04-28 10:11:18 -0400

    Process end: setting up look and feel of components, diving into logic of todo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from "react";
 import "./App.css";
+
+//Component imports
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <header>
+        <h1>List your To-Do's</h1>
+      </header>
+      <Form />
+      <TodoList />
     </div>
   );
 }
